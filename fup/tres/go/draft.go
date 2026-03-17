3b65457cbfa86481a74c26fadfab5dbc68af4493
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-03-17 18:20:20 +0000

    somei

--- fup/tres/go/draft.go
+++ fup/tres/go/draft.go
@@ -1,5 +1,12 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var a = 2
+    var b = 3
+    var c = 4
+    fmt.Scan(&a)
+    fmt.Scan(&b)
+    fmt.Scan(&c)
+    resultado := a + b + c
+    fmt.Println(resultado)
 }
