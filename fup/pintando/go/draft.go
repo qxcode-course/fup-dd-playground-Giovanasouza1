865850645028a56e6f270607b342ef6a21d110ea
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-03-28 18:19:07 +0000

    fiz pintando + bala

--- fup/pintando/go/draft.go
+++ fup/pintando/go/draft.go
@@ -1,8 +1,14 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"math"
+)
 func main() {
     var a, b, c float64
     fmt.Scan(&a, &b, &c)
+    p := (a + b + c)/2
+    Area := math.Sqrt(float64(p * (p - a) * (p - b) * (p - c)))
 
-    fmt.Println("qxcode")
+    fmt.Printf("%.2f\n", Area)
 }
