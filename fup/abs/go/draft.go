c0168bde57f131b94bd574f887e314a7ae5ffbda
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-03-31 13:54:17 +0000

    fiz abs e data

---
+++ fup/abs/go/draft.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+    var a, b int
+    fmt.Scan(&a, &b)
+    if a - b <0 {
+        fmt.Println((a - b) * -1)
+    } else {
+        fmt.Println(a - b)
+    }
+}
--- fup/data/go/draft.go
+++ fup/data/go/draft.go
@@ -3,6 +3,7 @@ import "fmt"
 func main() {
     var a, b, c, d, e int
     fmt.Scan(&a, &b, &c, &d, &e)
-    fmt.Printf("%.2d:%.2d\n", a, b)
-    fmt.Print(c / d / e)
+    fmt.Printf("%02d:%02d ", a, b)
+    fmt.Printf("%.2d/%.2d/%.2d\n", c, d, e % 100)
+
 }
