0cf0bd3b829ec8c6d8407d3e808cf431a6f89b5a
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-04-10 20:14:01 +0000

    tentei monica

--- fup/monica/go/draft.go
+++ fup/monica/go/draft.go
@@ -3,7 +3,14 @@ import "fmt"
 func main() {
     var m, a, b int
     fmt.Scan(&m, &a, &b)
-    monica := m + a + b
-    if 
-    fmt.Println("Hello, World!")
+    c := m - (a + b)
+    if c > a && c > b {
+        fmt.Println(c)
+    } else if a > b && a > m {
+        fmt.Println(a)
+    } else if b > a && b > m {
+        fmt.Println(b)
+    
+    }
+    
 }
