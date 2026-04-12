2b491aac88dab8b77f86960d80b2d8b36b1ced39
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-04-12 18:21:27 +0000

    tentei jokenpo

---
+++ fup/pr_jokenpo/go/draft.go
@@ -0,0 +1,42 @@
+package main
+import "fmt"
+func main() {
+    const PEDRA int = 0
+    const PAPEL int = 1
+    const TESOURA int = 2
+    const LAGARTO int = 3
+    const SPOCK int = 4
+
+
+    var jog1, jog2 int
+    var vit1, vit2 int
+    fmt.Scan(&vit1, &vit2)
+    for {
+        if vit1 > 3 && vit2 > 3 {
+             break }
+    
+
+
+    fmt.Print("Jog1: digite 0(pedra), 1(papel, 2(tesoura), 3(lagarto), 4(spock):")
+    fmt.Scan(&jog1)
+    fmt.Print("Jog2: digite 0(pedra), 1(papel, 2(tesoura), 3(lagarto), 4(spock):")
+    fmt.Scan(&jog2)
+    fmt.Print(jog1, jog2)
+    fmt.Print("Placar vit1: ", vit1, "Placar vit2: ", vit2)
+
+    if jog1 == jog2 {
+        fmt.Println("empate")
+    } else if (jog1 == 0 && jog2 == 3) || (jog1 == 0 && jog2 == 2) || (jog1 == 1 && jog2 == 4) || (jog1 == 1 && jog2 == 0) || (jog1 == 2 && jog2 == 3) || (jog1 == 2 && jog2 == 1) || (jog1 == 3 && jog2 == 1) || (jog1 == 3 && jog2 == 4) || (jog1 == 4 && jog2 == 0) || (jog1 == 4 && jog2 == 2){
+        fmt.Println("jog1 ganhou!")
+        vit1 += 1
+    } else {
+        fmt.Println("jog2 ganhou!")
+        vit2 += 1
+    }
+} ; 
+if vit1 > vit2 {
+    fmt.Println("Jog1 ganhou o jogo!!")
+} else {
+    fmt.Println("jog2 ganhou o jogo!!")
+}
+}
