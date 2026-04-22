ced0231f34416ba2447dd7f58756293b3d1e6198
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-04-22 13:28:48 +0000

    fiz loop ii

--- fup/loop_ii/src/go/draft.go
+++ fup/loop_ii/src/go/draft.go
@@ -3,8 +3,10 @@ import "fmt"
 func main() {
     var a, b int
     fmt.Scan(&a, &b)
-    for a, valor := ; a <= b; a++ {
-       if a != 0 {
-       } fmt.Print(a)
+    fmt.Print("[ ")
+    for i := a; i < b; i++{
+        fmt.Printf("%d ", i)
+    } 
+    fmt.Println("]")
     }
-}
+
