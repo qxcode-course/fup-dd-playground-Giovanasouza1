c2e511fd3e66d9781aef188b32461f8fb10ee0a2
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-05-01 15:07:31 +0000

    não consegui entender o break e continue

---
+++ fup/loop_v/src/go/draft.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main() {
+    var a, b int
+    fmt.Scan(&a, &b)
+    fmt.Print("[ ")
+    for i := a; i <= b; i++ {
+        if i % 2 == 0 {
+            continue
+        } else if i % 2 == 1 {
+            fmt.Printf("%d", i)
+        }; else if i == b { 
+            break
+        }
+    }
+    fmt.Println("]")
+}
