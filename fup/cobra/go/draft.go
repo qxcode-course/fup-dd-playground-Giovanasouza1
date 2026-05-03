fc5226c1a0a1acf970f15a7c52d4ed7bc641dbea
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-05-03 18:26:35 +0000

    :/

--- fup/cobra/go/draft.go
+++ fup/cobra/go/draft.go
@@ -1,15 +1,19 @@
 package main
 
-import ("fmt"; "math")
+import "fmt"
 
 func main() {
 	var n, x, y, s int
 	var c string
 	fmt.Scan(&n, &x, &y, &s, &c)
-	if c == "R" {
-		fmt.Println("x" + "s")
+	if c == "R"  {
+		fmt.Println(((x + s) % n), y)
+	} else if c == "U" {
+		fmt.Println(x, ((y - s + n) % n))
+	} else if c == "L" {
+		fmt.Println(((x + s) % n), y)
+	} else if c == "D" {
+		fmt.Println(x, ((y + s) % n))
 	}
-
-
 }
 
