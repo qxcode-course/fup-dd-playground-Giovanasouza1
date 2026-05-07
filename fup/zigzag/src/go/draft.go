dcb88376f994e09fd94406d0a1b38cf2e7dd59ee
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-05-07 11:02:11 +0000

    eu amo if else

--- fup/zigzag/src/go/draft.go
+++ fup/zigzag/src/go/draft.go
@@ -4,9 +4,16 @@ func main() {
     var a, z int
     fmt.Scan(&a, &z)
     for i := a; i <= z; i ++ {
-        if i % 3 == 0|| i % 5 == 0 {
-            fmt.Println()
-        }
+        if i % 3 == 0 && i % 5 != 0{
+            fmt.Println("zig") 
+        } else if i % 5 == 0 && i % 3 != 0{
+            fmt.Println("zag")
+        } else if i % 3 == 0 && i % 5 == 0 {
+            fmt.Println("zigzag")
+        } else if i % 3 != 0 {
+            fmt.Println(i)
+        } else if i % 5 != 0 {
+            fmt.Println(i)
+        } 
     }
-    fmt.Println("Hello, World!")
 }
