a8ddfe054795f0eb3afab1af6e45af301ca5d75d
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-05-10 19:48:06 +0000

    .

--- fup/colorido/src/go/draft.go
+++ fup/colorido/src/go/draft.go
@@ -1,5 +1,22 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
-}
+    var N int
+    var lado string
+    fmt.Scan(&N, &lado)
+    //passos//
+    fmt.Print("[ ")
+    for i := 0; i <= 9; i++{
+        if i != N {
+            fmt.Print(i, " ")
+        } 
+    lado == "d" || lado == "e"
+    if i % 2 == 0 {
+        fmt.Println("d")
+    } else {
+        fmt.Println("e")
+    }
+    fmt.Print("ceu ]")
+     }
+
+    }
