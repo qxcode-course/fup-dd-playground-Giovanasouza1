5d7f869da629b24b093874b26f8e8286bdbc7e6c
Giovanasouza1 <giovanasouza@alu.ufc.br>
2026-05-26 01:35:01 +0000

    tentei

--- fup/baruel/src/go/draft.go
+++ fup/baruel/src/go/draft.go
@@ -1,6 +1,23 @@
 package main
 import "fmt"
 func main() {
-    var nums []int = make([]int, 0, 1)
-    fmt.Println()
+    var talbum, tbaruel, tsequencia int
+    fmt.Scan(&talbum, &tbaruel, &tsequencia)
+
+    album := make([]int, talbum)
+    baruel:= make([]int, tbaruel)
+    sequencia := make([]int, tsequencia)
+
+    for a := 0; a < talbum; a++ {
+        fmt.Scan(&album[a])
+    }
+
+    for b := 0; b < tbaruel; b++ {
+        fmt.Scan(&baruel[b])
+    }
+
+    for s := 0; s < b ; s++ {
+        fmt.Scan(&sequencia[s])
+    }
+
 }
--- fup/batraquios/src/go/draft.go
+++ fup/batraquios/src/go/draft.go
@@ -3,19 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var N, M int
+	var N int
 	fmt.Scan(&N)
 	primeiro := make([]int, N)
     for i := 0; i < N; i++ {
 		fmt.Scan(&primeiro[i])
 	}
-    
+    var M int
+	fmt.Scan(&M)
 	segundo := make([]int, M)
-
-
 	for j := 0; j < M; j++ {
 		fmt.Scan(&segundo[j])
 	}
+
+
 	lista1 := 0
 	for i := 0; i < N; i++ {
 		for j := 0; j < M; j++ {
